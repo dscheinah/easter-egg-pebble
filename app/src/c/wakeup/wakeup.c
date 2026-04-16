2f96d892ef1844ee01f150628198959ef75a58df
dscheinah <github@jb.chmann.net>
2026-04-16 21:54:05 +0200

    feat: add app glance to peak stats

--- app/src/c/wakeup/wakeup.c
+++ app/src/c/wakeup/wakeup.c
@@ -3,11 +3,57 @@
 
 #define MAX_TRY 23
 #define TIMEOUT 30000
+#define BUFFER_SIZE 32
 
 static State* global;
 
+static char buffer[BUFFER_SIZE] = "";
+
 static void timeout() {
   window_stack_pop_all(false);
+}
+
+static void app_glance(AppGlanceReloadSession* session, size_t limit, void* context) {
+  if (limit < 1) {
+    return;
+  }
+
+  char quest[8] = "";
+  int done = 0;
+  switch (global->quest) {
+    case QUEST_STEPS:
+      strncpy(quest, "Steps", 8);
+      done = global->quest_target_done * 100 / global->quest_target;
+      break;
+    case QUEST_ACTIVE:
+      strncpy(quest, "Active", 8);
+      done = global->quest_target_done * 100 / global->quest_target;
+      break;
+    case QUEST_SLEEP:
+      strncpy(quest, "Sleep", 8);
+      done = global->quest_target_done * 100 / global->quest_target;
+      break;
+    case QUEST_RESTFUL:
+      strncpy(quest, "Restful", 8);
+      done = global->quest_target_done * 100 / global->quest_target;
+      break;
+    case QUEST_STEPS_TIME:
+    case QUEST_ALL_TIME:
+      strncpy(quest, "Timer", 8);
+      done = global->quest_duration_done * 100 / global->quest_duration;
+      break;
+  }
+
+  snprintf(buffer, BUFFER_SIZE, "@%d - %s %d%%", global->level, quest, done);
+
+  const AppGlanceSlice slice = {
+    .layout = {
+      .icon = APP_GLANCE_SLICE_DEFAULT_ICON,
+      .subtitle_template_string = buffer,
+    },
+    .expiration_time = APP_GLANCE_SLICE_NO_EXPIRATION,
+  };
+  app_glance_add_slice(session, slice);
 }
 
 static void wakeup_handler() {
@@ -26,6 +72,8 @@ static void wakeup_handler() {
 
 bool wakeup_init(State* state) {
   global = state;
+
+  app_glance_reload(app_glance, NULL);
 
   wakeup_service_subscribe(wakeup_handler);
   wakeup_handler();
