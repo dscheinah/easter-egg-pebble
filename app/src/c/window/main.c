d89970ed1fb2204f856b1b912c29ad222e53ffe2
dscheinah <github@jb.chmann.net>
2026-04-18 14:21:52 +0200

    refactor: window initialization

--- app/src/c/window/main.c
+++ app/src/c/window/main.c
@@ -26,40 +26,14 @@ static void render(Layer* layer, GContext* ctx) {
   graphics_fill_circle(ctx, GPoint(bounds.size.w + 2, bounds.size.h / 2), 10);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  global->seed--;
-  egg_render();
-}
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  Layer* layer = action_bar_layer_get_layer(action_bar);
-  bool hidden = !layer_get_hidden(layer);
-  layer_set_hidden(layer, hidden);
-  layer_set_hidden(text_layer_get_layer(text_layer), hidden);
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  global->seed++;
-  egg_render();
-}
-
-static void action_bar_click_config_provider(void *context) {
-  window_single_repeating_click_subscribe(BUTTON_ID_UP, 333, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 333, down_click_handler);
-}
-
-void main_init(State* state) {
-  global = state;
-  egg_init(state);
-
-  int exp = global->exp / global->level;
-
+static void update_buffer() {
   int done = global->quest_target_done;
   int target = global->quest_target;
+
   char quest[8] = "";
   char unit[4] = "";
   char counter[10] = "";
+
   switch (global->quest) {
     case QUEST_STEPS:
       strncpy(quest, "Steps", 8);
@@ -100,7 +74,7 @@ void main_init(State* state) {
 
   snprintf(buffer, BUFFER_SIZE, "Level %d\nExp %d%%\nQuest %s\n%d/%d %s\n%s",
     global->level,
-    exp,
+    global->exp / global->level,
     quest,
     done,
     target,
@@ -109,11 +83,43 @@ void main_init(State* state) {
   );
 }
 
+static void toggle_status(bool hidden) {
+  if (!hidden) {
+    update_buffer();
+  }
+  layer_set_hidden(action_bar_layer_get_layer(action_bar), hidden);
+  layer_set_hidden(text_layer_get_layer(text_layer), hidden);
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  global->seed--;
+  egg_render();
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  toggle_status(!layer_get_hidden(action_bar_layer_get_layer(action_bar)));
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  global->seed++;
+  egg_render();
+}
+
+static void action_bar_click_config_provider(void *context) {
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, 333, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 333, down_click_handler);
+}
+
+
+void main_init(State* state) {
+  global = state;
+  egg_init(state);
+}
+
 void main_load(Window* window) {
   egg_load(window);
   egg_render();
-
-  bool hide_status = global->result != HEALTH_RESULT_QUEST;
 
   Layer* window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -130,15 +136,14 @@ void main_load(Window* window) {
   action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, action_bar_icon_dismiss);
   action_bar_layer_set_icon_press_animation(action_bar, BUTTON_ID_SELECT, ActionBarLayerIconPressAnimationNone);
   action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_DOWN, action_bar_icon_down, true);
-
-  layer_set_hidden(action_bar_layer_get_layer(action_bar), hide_status);
   action_bar_layer_add_to_window(action_bar, window);
 
   GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_14);
   int start = PBL_IF_RECT_ELSE(PBL_DISPLAY_WIDTH / 12, PBL_DISPLAY_WIDTH / 6);
   int size = 85;
   text_layer = helper_text_layer_create(window_layer, GRect(start, start, size, size), font, buffer);
-  layer_set_hidden(text_layer_get_layer(text_layer), hide_status);
+
+  toggle_status(global->result != HEALTH_RESULT_QUEST);
 }
 
 void main_unload() {
